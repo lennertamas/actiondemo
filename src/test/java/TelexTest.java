68f811e4198e2fab2bd6366c11d51d89df8c9176
lennertamas <tamas.lenner@gmail.com>
2022-05-31 10:14:24 +0200

    second push

--- src/test/java/TelexTest.java
+++ src/test/java/TelexTest.java
@@ -1,9 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.ByteArrayInputStream;
 import java.util.concurrent.TimeUnit;
 
 public class TelexTest {
@@ -11,6 +15,10 @@ public class TelexTest {
     public WebDriver driver;
 
     @Test
+    @Epic("Telex")
+    @Story("Telex Landing Page")
+    @Description("Navigation to the landing page")
+    @Severity(SeverityLevel.CRITICAL)
     public void TestNavigation()
     {
         WebDriverManager.chromedriver().setup();
@@ -27,5 +35,6 @@ public class TelexTest {
         driver.manage().window().maximize();
 
         driver.navigate().to("https://telex.hu");
+        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
     }
 }
